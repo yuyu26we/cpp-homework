0b86338d440951fa9170c0c2c78eb3c0b3f70ba9
yuyu26we <kva.std26@gmail.com>
2024-09-22 20:48:39 +0500

    hw01 edit

--- hw01/MultiplicationTable/MultiplicationTable/MultiplicationTable.cpp
+++ hw01/MultiplicationTable/MultiplicationTable/MultiplicationTable.cpp
@@ -1,6 +1,31 @@
-﻿ #include <iostream>
+﻿#include <iostream>
+
+const int mTableRows = 8;
+const int mTableCols = 8;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int multiplicationTable[mTableRows][mTableCols];
+	int firstNum = 0;
+	int secondNum = 0;
+
+	for (int i = 0; i < mTableRows; i++)
+		for (int j = 0; j < mTableCols; j++)
+			multiplicationTable[i][j] = (i + 2) * (j + 2);
+
+	/*for (int i = 0; i < mTableRows; i++) {
+		for (int j = 0; j < mTableCols; j++)
+		{
+			std::cout << multiplicationTable[i][j] << '\t' << ' ';
+		}
+		std::cout << '\n';
+	}*/
+
+	std::cout << "Write 1st num: ";
+	std::cin >> firstNum;
+
+	std::cout << "Write 2st num: ";
+	std::cin >> secondNum;
+
+	std::cout << "Result: " << multiplicationTable[firstNum - 2][secondNum - 2];
 }
