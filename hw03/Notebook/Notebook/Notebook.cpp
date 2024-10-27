d650a84a43460112daa403956ac86a3ef5b5cab7
yuyu26we <kva.std26@gmail.com>
2024-10-27 23:38:09 +0500

    hw03 edit

---
+++ hw03/Notebook/Notebook/Notebook.cpp
@@ -0,0 +1,56 @@
+﻿#include "Notebook.h"
+
+int main()
+{
+    vector<Note> notebook {};
+    CreateNote(notebook);
+
+
+}
+
+void CreateNote(vector<Note>& notebook)
+{
+    Note newNote;
+
+    while (true)
+    {
+        string stringedNum;
+
+        try
+        {
+            printf("Enter the mobile number of the contact (Nums Only): ");
+            cin >> stringedNum;
+
+            newNote.mobileNumber = stoi(stringedNum);
+            break;
+        }
+        catch (invalid_argument) 
+        {
+            printf("Incorrect input!\n");
+        }
+    }
+
+    printf("Enter the name of the contact (Strings Only): ");
+    cin >> newNote.name;
+
+    printf("Enter the surname of the contact (Strings Only): ");
+    cin >> newNote.surname;
+
+    while(true) 
+    {
+        string gender;
+        printf("Enter the gender of the contact (Male/Famale): ");
+
+        cin >> gender;
+
+        if (gender == "Male" || gender == "Female") { break; }
+        else { printf("Incorrect input!\n"); }
+    }
+
+    notebook.push_back(newNote);
+}
+
+Note& SearchNote(vector<Note>& notebook) 
+{
+
+}
---
+++ hw03/Notebook/Notebook/Notebook.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+enum Gender
+{
+	Male,
+	Female
+};
+
+struct Note
+{
+	int mobileNumber;
+	string name;
+	string surname;
+	Gender gender;
+};
+
+void CreateNote(vector<Note>& notebook);
+Note& SearchNote(vector<Note>& notebook);
