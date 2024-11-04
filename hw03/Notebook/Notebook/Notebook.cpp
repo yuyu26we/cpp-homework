74902f6576b51e3f227129c5a4489b836d448f96
yuyu26we <kva.std26@gmail.com>
2024-11-04 17:24:33 +0500

    hw03 edit

--- hw03/Notebook/Notebook/Notebook.cpp
+++ hw03/Notebook/Notebook/Notebook.cpp
@@ -8,13 +8,24 @@ int main()
     {
         string answer;
 
-        printf("Write new contact\n");
+        printf("| Write new contact |\n\n");
         CreateNote(notebook);
 
-        printf("Add one more contact? (y/n): ");
-        cin >> answer;
-        if (answer == "y") { continue; }
-        else if (answer == "n") { break; }
+        while (true) 
+        {
+            printf("Add one more contact? (y/n): ");
+            cin >> answer;
+            printf("\n");
+
+            if (answer != "y" && answer != "n") 
+            {
+                printf("Incorrect input!\n\n");
+                continue;
+            }
+            break;
+        }
+
+        if(answer == "n") { break; }
     }
 
     SearchNotes(notebook);
@@ -30,33 +41,57 @@ void CreateNote(vector<Note>& notebook)
 
         try
         {
-            printf("Enter the mobile number of the contact (Nums Only): ");
+            printf("Enter the mobile number of the contact(Num must consist of 9 digits): ");
             cin >> stringedNum;
 
+            if (stringedNum.length() != 9) 
+            {
+                printf("Mobile number must consist of 9 digits!\n");
+                continue;
+            }
             newNote.mobileNumber = stoi(stringedNum);
             break;
         }
         catch (invalid_argument) 
         {
-            printf("Incorrect input!\n");
+            printf("Incorrect input!\n\n");
         }
     }
 
-    printf("Enter the name of the contact (Strings Only): ");
+    printf("Enter the name of the contact: ");
     cin >> newNote.name;
 
-    printf("Enter the surname of the contact (Strings Only): ");
+    printf("Enter the surname of the contact: ");
     cin >> newNote.surname;
 
     while(true) 
     {
-        string gender;
-        printf("Enter the gender of the contact (Male/Famale): ");
+        string stringedGender;
+        int gender;
 
-        cin >> gender;
+        try
+        {
+            printf("Enter the gender of the contact (Male = 0 | Female = 1): ");
+            cin >> stringedGender;
 
-        if (gender == "Male" || gender == "Female") { break; }
-        else { printf("Incorrect input!\n"); }
+            printf("\n");
+
+            gender = stoi(stringedGender);
+
+            if (gender != 0 && gender != 1) 
+            {
+                printf("Incorrect input!\n\n");
+                continue;
+            }
+
+            newNote.gender = (Gender)gender;
+            break; 
+        }
+
+        catch (invalid_argument)
+        {
+            printf("Incorrect input!\n\n");
+        }
     }
 
     notebook.push_back(newNote);
@@ -66,26 +101,37 @@ void SearchNotes(vector<Note>& notebook)
 {
     string detail;
 
-    printf("Enter contact detail: ");
+    printf("Enter contact detail(Case sensitive): ");
     cin >> detail;
 
     bool isAtLeastOneFound = false;
 
-    printf("List of founded: ");
+    printf("\n");
+    printf("List of founded:\n\n");
+
+    int counter = 0;
 
     for (const Note& note : notebook)
     {
-        int counter = 0;
-        if (to_string(note.mobileNumber) == detail || note.name == detail || note.surname == detail) 
+        if (note.name == detail || note.surname == detail) 
         {
             counter++;
             isAtLeastOneFound = true;
-            printf("%i---------------", counter);
-            printf("Name: %s\n", note.name);
-            printf("Surname: %s\n", note.surname);
+
+            printf("No %i ---------------|\n", counter);
+            printf("Name: %s\n", note.name.c_str());
+            printf("Surname: %s\n", note.surname.c_str());
             printf("Mobile number: %i\n", note.mobileNumber);
             printf("Gender: %s\n", (note.gender == Gender::Male) ? "Male" : "Female");
-            printf("----------------");
+            printf("-------------------|\n");
         }
     }
+
+    if (counter == 0) 
+    { 
+        printf("-------------------------------------------|\n\n");
+        printf("No results found for the search query \"%s\"\n\n", detail.c_str()); 
+        printf("-------------------------------------------|\n");
+    }
+
 }
